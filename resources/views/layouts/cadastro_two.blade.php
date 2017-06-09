38845c40c4782812240af9318998c7dcf320701e
Matheus <mathiew6@gmail.com>
2017-06-09 12:00:34 -0300

    #4 #6 Alteração do título de 'Laravel' para 'Navi'

--- resources/views/layouts/cadastro_two.blade.php
+++ resources/views/layouts/cadastro_two.blade.php
@@ -9,7 +9,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Navi</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
