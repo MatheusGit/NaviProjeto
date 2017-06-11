954c111fc01151417e6e0b83819b1d481bce5403
Matheus <mathiew6@gmail.com>
2017-06-11 10:09:02 -0300

    #10 Criação e implementação do FormRequest que validará os dados do formulário

---
+++ app/Http/Requests/DadosFormRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class DadosFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'cpf' => 'nullable|between:11,11',
+            'rg' => 'nullable|between:9,9',
+            'datanasc' => 'nullable',
+            'genero' => 'nullable',
+            'outro' => 'nullable | string',
+            'cep' => 'nullable',
+            'numero' => 'nullable|numeric|min:0'
+        ];
+    }
+
+    public function messages(){
+        return [
+            'cpf.required' => 'O campo CPF é obrigatório',
+            'cpf.between' => 'O campo CPF deve conter 11 dígitos',
+            'rg.required' => 'O campo RG é obrigatório',
+            'RG.between' => 'O campo RG deve conter 9 dígitos',
+            'outro.string' => 'O campo Qual gênero? deve possui apenas letras',
+            'numero.numeric' => 'O campo Número deve conter apenas números',
+        ];
+    }
+}
