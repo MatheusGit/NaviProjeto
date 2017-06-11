f49b6aabd670caf2971179ead0ab793e1d20b1f9
Matheus <mathiew6@gmail.com>
2017-06-10 23:13:01 -0300

    #8 Instalação do 'intervention image', criação de diretório /uploads/avatar em public. Implementação no CRUD da lógica de troca de avatar.

--- app/Http/Controllers/CRUD.php
+++ app/Http/Controllers/CRUD.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Pessoal;
+use App\logins;
+use Image;
+use Auth;
 
 class CRUD extends Controller
 {
@@ -22,69 +25,16 @@ class CRUD extends Controller
         return view('show')->with('dados');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+    public function imagem(Request $request){
+        if($request->hasFile('avatar')){
+            $avatar = $request->file('avatar');
+            $filename = time() . '.' . $avatar->getClientOriginalExtension();
+            Image::make($avatar)->resize(300,300)->save(public_path('/uploads/avatar/'. $filename));
+            $user = Auth::guard('logins')->user();
+            $user->update(['avatar' => $filename]);
+        }
+
+        return redirect()->route('inicio');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        return 'store';
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
