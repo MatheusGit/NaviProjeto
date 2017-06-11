c9405c8ef150bca510237fe6ae21328a651c1681
Matheus <mathiew6@gmail.com>
2017-06-11 20:10:14 -0300

    #10 Adição das novas colunas ao método fillable da class App\Pessoal

--- app/Pessoal.php
+++ app/Pessoal.php
@@ -9,7 +9,8 @@ class Pessoal extends Model
     protected $table = 'pessoal';
 
     protected $fillable = [
-		'cpf', 'rg', 'datanasc', 'genero', 'numero', 'cep','complemento', 'login_id',
+		'cpf', 'rg', 'datanasc', 'genero', 'numero', 'cep','complemento', 'login_id', 'outro', 
+		'genero_select', 'complemento_select',
     ];
     
     public function logint(){
